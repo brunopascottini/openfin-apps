702a8bab5201068b9a9be624eac59de70bc9ebf7
brunopascottini <bruno.pascottini@gmail.com>
2020-10-12 16:03:27 +0100

    Updates

---
+++ precache-manifest.4aa9dfc8bca21df6b1d10fc22a73532f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a4b5a1a691babd41b114cc1dd2079f",
+    "url": "/openfin-apps/index.html"
+  },
+  {
+    "revision": "033d771c8aa6340e2ace",
+    "url": "/openfin-apps/static/css/main.e3dbca1a.chunk.css"
+  },
+  {
+    "revision": "27abf3efa283c006a4b8",
+    "url": "/openfin-apps/static/js/2.3c63f209.chunk.js"
+  },
+  {
+    "revision": "033d771c8aa6340e2ace",
+    "url": "/openfin-apps/static/js/main.2c4445d8.chunk.js"
+  },
+  {
+    "revision": "b0e6d45d1a1602c995b9",
+    "url": "/openfin-apps/static/js/runtime-main.d98edcba.js"
+  },
+  {
+    "revision": "b65122070db4e1b77aecdadd11095da4",
+    "url": "/openfin-apps/static/media/cancel.b6512207.svg"
+  },
+  {
+    "revision": "777b93c6d5a7b86081144f8d0d3ca1d8",
+    "url": "/openfin-apps/static/media/close.777b93c6.svg"
+  },
+  {
+    "revision": "e249914e9d3c8c92e0059c063c950353",
+    "url": "/openfin-apps/static/media/done.e249914e.svg"
+  },
+  {
+    "revision": "64de38475d6ea6a35c277c6873f7fa08",
+    "url": "/openfin-apps/static/media/step_back.64de3847.svg"
+  },
+  {
+    "revision": "ebcf00f4b55ed834e4c23d2d853b7538",
+    "url": "/openfin-apps/static/media/step_next.ebcf00f4.svg"
+  }
+]);
